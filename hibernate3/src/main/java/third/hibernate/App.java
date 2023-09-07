39268a88de2375123a244318e8ac592667d281a1
Ajitesh <Ajitesh@NLBP-50447>
2023-09-07 20:37:35 +0530

    first commit

---
+++ hibernate3/src/main/java/third/hibernate/App.java
@@ -0,0 +1,47 @@
+package third.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Date;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+public class App 
+{
+    public static void main( String[] args ) {
+    	//Create the student object.
+    	Users user = new Users();
+ 
+    	//Setting the object properties.
+    	user.setUserName("Vivek");
+    	user.setPassword("vivek@123");
+    	user.setCreateDate(new Date());
+    	user.setCreateUser("Jai");
+ 
+    	Transaction tx = null;
+    	//Get the session object.
+    	Session session = HibernateUtil.getSessionFactory().openSession();
+    	try{
+              //Start hibernate session.
+    	      tx = session.beginTransaction();
+ 
+              //Insert a new student record in the database.
+    	      session.save(user); 
+ 
+              //Commit hibernate transaction if no exception occurs.
+    	      tx.commit();
+    	      System.out.println("Saved Successfully.");
+    	  }catch (HibernateException e) {
+    	     if(tx!=null){
+                 //Roll back if any exception occurs. 
+    	         tx.rollback();
+    	     }
+    	     e.printStackTrace(); 
+    	  }finally {
+             //Close hibernate session.
+    	     session.close(); 
+    	  }
+    }
+}
